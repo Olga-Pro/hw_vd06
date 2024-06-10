f6fe9753b0a4c898800147272335621273d1a16e
Olga-Pro <oip_@inbox.ru>
2024-06-10 10:41:22 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,10 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request
 
 from app import app
 
-posts = []
 
 # Создание маршрута, поддерживающего методы GET, POST
 @app.route('/', methods=['GET', 'POST'])
-
-
 def index():
     if request.method == 'POST':
         name = request.form.get('name')
